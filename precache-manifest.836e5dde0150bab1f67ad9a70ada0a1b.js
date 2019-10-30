b811fdb5d52b7220db68398ee28fadb908680fdd
fchandel <fchandel@sfu.ca>
2019-10-29 19:47:24 -0700

    Updates

---
+++ precache-manifest.836e5dde0150bab1f67ad9a70ada0a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375fcbd42a7eced248b4084a60ef7fd7",
+    "url": "/employee-hours-calculator/index.html"
+  },
+  {
+    "revision": "74bb19a631857878b59d",
+    "url": "/employee-hours-calculator/static/css/2.2e152c32.chunk.css"
+  },
+  {
+    "revision": "e3ff4ce48a938a42ef63",
+    "url": "/employee-hours-calculator/static/css/main.c74706b4.chunk.css"
+  },
+  {
+    "revision": "74bb19a631857878b59d",
+    "url": "/employee-hours-calculator/static/js/2.4880ce24.chunk.js"
+  },
+  {
+    "revision": "e3ff4ce48a938a42ef63",
+    "url": "/employee-hours-calculator/static/js/main.adcd5210.chunk.js"
+  },
+  {
+    "revision": "eaa6e7a4a2e5c0e1d015",
+    "url": "/employee-hours-calculator/static/js/runtime-main.3306fd01.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/employee-hours-calculator/static/media/logo.25bf045c.svg"
+  }
+]);
